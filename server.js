3954c28398598ebef80c4fb8e6532b93f5c59b4c
Razin Syakib <muhammadrazin14@gmail.com>
2023-03-17 15:48:17 +0700

    Menambahkan Response Status

--- server.js
+++ server.js
@@ -4,41 +4,21 @@ const http = require('http');
  
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
- 
-    // const { method } = request;
- 
-    // if(method === 'GET') {
-    //     response.end('<h1>Hello!</h1>');
-    // }
- 
-    // if(method === 'POST') {
-    //     let body = [];
-        
-    //     request.on('data', (chunk) => {
-    //         body.push(chunk);
-    //     });
-        
-    //     request.on('end', () => {
-    //         body = Buffer.concat(body).toString();
-    //         const { name } = JSON.parse(body);
-    //         response.end(`<h1>Hai, ${name}!</h1>`);
-    //     });
-    // }
-
-    // response.setHeader('Content-Type', 'text/html');
     // response.statusCode = 200;
  
     const { method, url } = request;
  
     if(url === '/') {
         if(method === 'GET') {
+            response.statusCode = 200;
             response.end('<h1>Ini adalah homepage</h1>');
         } else {
+            response.statusCode = 400;
             response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
         }
     } else if(url === '/about') {
         if(method === 'GET') {
+            response.statusCode = 200;
             response.end('<h1>Halo! Ini adalah halaman about</h1>');
         } else if(method === 'POST') {
             let body = [];
@@ -50,12 +30,15 @@ const requestListener = (request, response) => {
             request.on('end', () => {
                 body = Buffer.concat(body).toString();
                 const {name} = JSON.parse(body);
+                response.statusCode = 200;
                 response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
             });
         } else {
+            response.statusCode = 400;
             response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
         }
     } else {
+        response.statusCode = 400;
         response.end('<h1>Halaman tidak ditemukan!</h1>');
     }
 };
